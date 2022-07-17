249e49db71a4fec7d3a10faa93765f74457e71c3
workordeath <alfonso.oramas@icloud.com>
2022-07-17 19:50:04 -0400

    Added initial value output list to the console window

--- tarkovExperiment/tarkovExperimentProgram.cpp
+++ tarkovExperiment/tarkovExperimentProgram.cpp
@@ -24,3 +24,38 @@ public:
     void writeToData();
     void getDifference();
 }
+
+
+
+
+/*
+initialDataSet()
+================
+this data set takes the initial values
+from the start of the experiment
+and outputs them first.
+==================
+*/
+void dataSet::initialDataSet(){
+    //local variables.
+    const double WIDTH = 18;
+    const char FILL = ' ';
+    const char LINE_FILL = '=';
+    //introduction
+    cout << setw(WIDTH * 3.5) << "TARKOV SURVIVAL EXPERIMENT\n";
+    cout << setw(WIDTH * 5) << setfill(LINE_FILL) << "==========================\n";
+
+    cout << left << setw(WIDTH) << setfill(FILL) << "CASH";
+    cout << left << setw(WIDTH) << setfill(FILL) << "TOTAL RAIDS";
+    cout << left << setw(WIDTH) << setfill(FILL) << "SURVIVED";
+    cout << left << setw(WIDTH) << setfill(FILL) << "KIA";
+    cout << left << setw(WIDTH) << setfill(FILL) << "SURVIVAL RATE%";
+    cout << left << setw(WIDTH) << setfill(FILL) << "DATE" << endl;
+    //output from private member variable
+    cout << left << setw(WIDTH) << setfill(FILL) << initialCash;
+    cout << left << setw(WIDTH) << setfill(FILL) << initialTotalRaids;
+    cout << left << setw(WIDTH) << setfill(FILL) << initialSurvived;
+    cout << left << setw(WIDTH) << setfill(FILL) << initialKilledInAction;
+    cout << left << setw(WIDTH) << setfill(FILL) << initialSurvivalRate;
+    cout << left << setw(WIDTH) << setfill(FILL) << initialDate << endl;
+}
