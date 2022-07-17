c09453799e6f09385252420c301183c157f20b17
workordeath <alfonso.oramas@icloud.com>
2022-07-17 19:48:34 -0400

    Added a class to the program

--- tarkovExperiment/tarkovExperimentProgram.cpp
+++ tarkovExperiment/tarkovExperimentProgram.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-int main(){
-    cout << "Hello neighbor" << endl;
-    return 0;
+class dataSet{
+private:
+    const double initialCash = 546697;
+    const int initialSurvived = 7;
+    const int initialKilledInAction = 5;
+    const int initialTotalRaids = 12;
+    const double initialSurvivalRate = 0.58;
+    const string initialDate = "07/03/2022";
+private:
+    int presentCash;
+    int presentKIA;
+    double presentTotalRaids;
+    double presentSurvived;
+    double presentSurvivalRate;
+    string presentDate;
+public:
+    void initialDataSet();
+    void readFromData();
+    void writeToData();
+    void getDifference();
 }
